85968cf1217b140d608fffacf0cf6915dce48364
Jonas <emmanuel.jebt@gmail.com>
2017-01-24 11:16:09 -0600

    Registro

---
+++ js/init_js.js
@@ -0,0 +1,8 @@
+var config = {
+    apiKey: "AIzaSyD2_21Lv0eWbqgCQpDSvTgHH8Thp9uNaHg",
+    authDomain: "olimpiadapotosinadeinfor-49483.firebaseapp.com",
+    databaseURL: "https://olimpiadapotosinadeinfor-49483.firebaseio.com",
+    storageBucket: "olimpiadapotosinadeinfor-49483.appspot.com",
+    messagingSenderId: "810470494434"
+};
+firebase.initializeApp(config);
